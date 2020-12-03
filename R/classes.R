84c2a2b590226fb31ce054e66c0bc186ea022fec
Matt Espe <mespe@ucdavis.edu>
2020-12-03 15:31:37 -0700

    Basic classes.

---
+++ R/classes.R
@@ -0,0 +1,16 @@
+##' Telemetry data
+##'
+##' An S4 object holding the relevant data for a set of telemetry
+##' detections
+##' 
+##' @slot detections
+##' @slot tagging
+##' @slot deployment
+##' @slot quality_control_status
+##' @export
+setClass("telemetry",
+         slots = c(detections = "data.frame",
+                   tagging = "data.frame",
+                   deployment = "data.frame",
+                   quality_control_status = "logical"))
+
