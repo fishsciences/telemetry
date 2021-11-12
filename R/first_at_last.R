1740b287aad1a2dda21da4bb1ce36cd6432383c5
Myfanwy <myfanwy@github.com>
2021-11-12 13:40:27 -0800

    refactor first_at_last + tests

---
+++ R/first_at_last.R
@@ -0,0 +1,54 @@
+#' Subset to first (minimum) and last (maximum) detection and stations of a tagged animal
+#'
+#'@param dets_df A data frame of detections for one or more tags
+#'@param tagid_col Quoted name of column corresponding to the Tag Identification (TagID, Transmitter, etc)
+#'@param datetime_col Name of the column (in quotes) corresponding to detection date and time. Default is "DateTimeUTC". Column must be in POSIXct ymd_hm or ymd_hms format.
+#'
+#'@return a subset of the original dataframe with two rows for each fish (the top row is the minimum detection for that fish, the bottom row is the maximum detection)
+#'@details in cases where the fish only has one detection associated, or where the first and last detections represent simultaneous detections, only one row will be returned.
+#' 
+#'@author Myfanwy Johnston
+#'@export
+#'
+#'
+first_and_last <- function(dets_df, 
+                           tagid_col = "TagID",
+                           datetime_col = "DateTimeUTC") {
+
+  f1 <- split(dets_df, dets_df[[tagid_col]])
+  
+  tmp <- lapply(f1, fl_onefish, 
+                datetime = datetime_col)
+  
+  fldf = do.call(rbind, tmp)
+                 
+  return(fldf) }
+
+
+# internal function - does operation for one fish
+
+fl_onefish = function(df, 
+                      datetime = "DateTimeUTC") {
+  
+  # check
+  if(!is.data.frame(df)) stop("input data must be of class 'data.frame'")
+  
+  dt_col = df[[datetime]] # assign datetime column 
+    if(!is(dt_col, "POSIXct")) stop("Datetime column is not POSIXct")
+
+  firstlast =   
+    
+                    # repalce rbind with this-ish eventually
+                    # df[c(which.min(dt_col), 
+                    #  which.max(dt_col)), ]
+                
+                rbind( # could replace with which.min and which.max, and replace rbind with indexing []
+    
+                df[dt_col == min(dt_col), ], # min det
+                df[dt_col == max(dt_col), ] # max det
+                
+                )
+  
+  firstlast[!duplicated(firstlast[[datetime]]), ] # could be slow
+  
+}
--- tests/test_first_last.R
+++ tests/test_first_last.R
@@ -1,38 +1,39 @@
 # test fl_onefish
 library(tools)
+library(telemetry)
 
-ac = readRDS("inst/ac_test.rds")
+ac = readRDS(system.file(package = "telemetry", "ac_test.rds")) # makes sure that any system that has installed the package can find this
 
 ac_test = ac[ac$Transmitter == ac$Transmitter[1], ]
 ac_test2 = ac[ac$Transmitter == unique(ac$Transmitter)[2], ]
 
 #-------------------------------------------------------#
-# fl_onefish tests
+# :::fl_onefish tests
 #-------------------------------------------------------#
 
 # does it actually return the min and max detection?
-x = fl_onefish(ac_test2)
+x = telemetry:::fl_onefish(ac_test2)
 stopifnot(x$DateTimeUC[1] == min(ac_test2$DateTimeUTC))
 stopifnot(x$DateTimeUTC[2] == max(ac_test2$DateTimeUTC))
 
 # returns two rows?
-stopifnot(nrow(fl_onefish(ac_test)) == 2)
+stopifnot(nrow(telemetry:::fl_onefish(ac_test)) == 2)
 
 #  test input conditions
 # datetimecol should be POSIXct
-assertError(fl_onefish(ac_test, "Transmitter"))
+assertError(telemetry:::fl_onefish(ac_test, "Transmitter"))
 
 # input should be a data.frame
-assertError(fl_onefish(df = 1:5, "DateTimeUTC"))
-assertError(fl_onefish(df = "x", "DateTimeUTC"))
+assertError(telemetry:::fl_onefish(df = 1:5, "DateTimeUTC"))
+assertError(telemetry:::fl_onefish(df = "x", "DateTimeUTC"))
 
 # when there are simultaneous detections, should still return 2 rows
 sims = rbind(ac_test, ac_test)
-stopifnot(nrow(fl_onefish(sims, "DateTimeUTC")) == 2)
+stopifnot(nrow(telemetry:::fl_onefish(sims, "DateTimeUTC")) == 2)
 
 # when a fish has 1 detection, should return a single row:
 one = ac_test[1, ]
-stopifnot(nrow(fl_onefish(one)) == 1)
+stopifnot(nrow(telemetry:::fl_onefish(one)) == 1)
 
 
 #-------------------------------------------------------#
