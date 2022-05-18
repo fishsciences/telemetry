b6476a051b9d21fd96cf2ebe31bd905add521441
Myfanwy <myfanwy.johnston@fishsciences.net>
2022-05-18 15:00:46 -0700

    first draft of div_dist fxn, lots of little fixes

---
+++ R/div_dist.R
@@ -0,0 +1,27 @@
+#' Divide distance evenly across a units of a time span
+#'
+#' @param start 
+#' @param end 
+#' @param distance 
+#' @param time_units 
+#'
+#' @return data frame with columns \itemize{
+#'   \item date_time: POSIXct column of date/time stamps in the units specified by time_units
+#'   \item prop_dist: the proportion of the distance allocated to each `date_time` row
+#'   }
+#' 
+#' @export
+#'
+
+div_dist = function(start, end, distance, time_units = "hours") {
+  
+  v = seq.POSIXt(from = start, to = end, 
+                 by = time_units)
+  data.frame(date_time = v,
+             prop_dist = rep(distance/length(v), length(v)))
+  
+}
+
+#' @examples
+#' tt = as.POSIXct(c("2020-01-01 00:00:00", "2020-01-02 12:00:00"))
+#' div_dist(tt[1], tt[2], distance = 10)
---
+++ tests/test_div_dist.R
@@ -0,0 +1,32 @@
+d = readRDS("inst/ac_test.rds") |>
+  dplyr::select(Transmitter, StationName, DateTimeUTC) |>
+  tag_tales(TagID_col = "Transmitter", Station_col = "StationName", "DateTimeUTC", allow_overlap = FALSE)
+
+dd = d[d$Transmitter == unique(d$Transmitter)[1], ] # take down to 1 fish to test
+dd = dd[1:5, c("Transmitter", "StationName", "arrival", "departure")] # cut down rows and cols needed
+
+# make a column of lagged arrivals so that the indexing with div_dist is easier
+dd$first_arrs = data.table::shift(dd$arrival, type = "lead") # is there a way to do this in base?
+
+str(dd)
+
+dd = dd[!is.na(dd$first_arrs), ] # remove last row, as the arrival at last receiver is now on the second-to-last row
+dd$distance = 2:5
+
+
+ans = lapply(seq(nrow(dd)), function(i) div_dist(start = dd$departure[i],  # I don't know about this
+                                           end = dd$first_arrs[i], 
+                                           distance = dd$distance[i], 
+                                           time_units = "hours"))
+
+ans2 = do.call(rbind, ans)
+sum(dd$distance) == sum(ans2$prop_dist)
+
+ans2$date = as.Date(ans2$date_time)
+
+tapply(ans2$prop_dist, ans2$date, sum)
+
+ans3 = do.call(rbind, mapply(div_dist, dd$departure, dd$first_arrs, dd$distance, SIMPLIFY = FALSE))
+ans3$date = as.Date(ans3$date_time)
+all.equal(ans2, ans3)
+
