70bc8977ddbf618c6fa680f084a5289e0f74a351
Myfanwy <myfanwy.johnston@fishsciences.net>
2022-06-27 12:03:32 -0700

    first draft refactor, needs cleanup

--- R/tag_tales.R
+++ R/tag_tales.R
@@ -1,40 +1,51 @@
 
+# function operates on a dataframe of a single station.  The rows in the data frame may or may not need to be split up further according to the threshold.
 
 redRowFun =
   function(d, dtc1)
   {
-    r = as.POSIXct(range(d[[dtc1]]))
-    data.frame(d[1,],
-               arrival = r[1],
-               departure = r[2],
+    
+    # vector of change points from a logical vector of rows whose diff > TimeThresh; every time it increments, that represents the row that should become a new station visit
+    if(nrow(d) > 1) {
+      breakup_vector = cumsum(c(0, diff(d[[dtc2]])) > TimeThreshold ) # vector
+      
+      # Example:
+      # set.seed(1)
+      # x = runif(6)
+      # thresh = 0.25
+      # abs(diff(x)) > thresh # should be F, F, T, T, T
+      # bv = cumsum(c(0, abs(diff(x))) > thresh) # should be: 0, 0, 0, 1, 2, 3; i.e., rows, 5, and 6 should have their own visits
+      if(any(breakup_vector)) {
+        tmp = by(d, breakup_vector, redRowFun, dtc1)
+        return(do.call(rbind, tmp)) 
+      }
+    }
+    # default:      
+    r = as.POSIXct(range(d[[dtc1]])) # first and last detection
+    data.frame(d[1,], # use first row with all its columns
+               arrival = r[1], # add arrival time
+               departure = r[2], # and departure time
                stringsAsFactors = FALSE)
+    
   }
 
 splitFishStationVisits =
-    function(d, Station_col,
-             TimeThreshold = Threshold,
-             rowFunc = redRowFun,
-             dtc2 = Datetime_col, overlap = TRUE)
-{
+  function(d, 
+           Station_col,
+           TimeThreshold = Threshold,
+           rowFunc = redRowFun,
+           dtc2 = Datetime_col)
+  {
     j = order(d[[dtc2]])
-    d = d[ j , ] #order dataframe by DateTimeUTC
-
-    ## index of continuous times
-    g = cumsum( c(0, diff(d[[dtc2]])) > TimeThreshold )
-
-    ## index of both conditions
-    # make new index with unique visits if overlap == FALSE
-    if(!overlap){
-        ## index of continuous stations
-        rr = rle(Station_col[j])
-        i = rep(seq(along = rr$lengths), times = rr$lengths)
-        
-        g = paste(i, g)
-    }
+    d = d[ j , ] # order dataframe by DateTimeUTC
     
-    ans = by(d, g, rowFunc, dtc1 = dtc2) # apply redRowFun by the grouping variable g to the dataframe
+    ## index of continuous times
+    
+    g = rleid(Station_col[j])
+    
+    ans = by(d, g, rowFunc, dtc1 = dtc2) # apply redRowFun by the station visit ID to the dataframe
     do.call(rbind, ans) # bind that into a dataframe
-}
+  }
 
 #' Contstruct coherant individual movement paths from tag detection history dataframe
 #'
@@ -56,13 +67,32 @@ splitFishStationVisits =
 #'     within 2 hours of the first arrival are considered part of the
 #'     same "stay" at that receiver.
 #'
+#'Tagtales: should reflect the tag residence at each receiver, in chronological order.  
+#' For each fish:
+#'  
+#' 1. order by DateTime
+#' 2. split by station (assumption: stations are grouped appropriately)
+#' 3. order by Datetime within station
+#' 4. create an rleid of the station column
+#' 5. apply the redRowFun to each rleid
+#' 
+#' 4. if df consists of a single detection repeat the timestamp as both arrival and departure
+#' 5. if nrow(df) > 1, then:
+#'   - split the detection history into multiple visits at points where the difference between two detections is 
+#'   > threshold.
+#'        - this is done by calculating the diff between rows where the station ID changes
+#'
+#' 6. Use the first detection as "arrival", last detection as "departure" for each visit
 #'
 #' @return dataframe with fishpaths for each tagID
 #' @export
 #'
 
-tag_tales <- function(detdf, TagID_col, Station_col,
-                      Datetime_col="DateTimeUTC", Threshold = 60*60,
+tag_tales <- function(detdf, 
+                      TagID_col, 
+                      Station_col,
+                      Datetime_col = "DateTimeUTC", 
+                      Threshold = 60*60,
                       allow_overlap = TRUE) {
 
     if(is.character(TagID_col) && length(TagID_col) != nrow(detdf))
@@ -70,10 +100,16 @@ tag_tales <- function(detdf, TagID_col, Station_col,
     if(is.character(Station_col) && length(Station_col) != nrow(detdf))
         Station_col = detdf[[Station_col]]
     
-    f1 <- split(detdf, TagID_col)
+   f1 <- split(detdf, TagID_col)
     
-  f1 <- f1[ sapply(f1, nrow) > 0 ]
-  tmp = lapply(f1, splitFishStationVisits, Station_col = Station_col, dtc2 = Datetime_col, TimeThreshold = Threshold, overlap = allow_overlap)
+   f1 <- f1[ sapply(f1, nrow) > 0 ]
+  
+  tmp = lapply(f1, splitFishStationVisits, 
+               Station_col = Station_col, 
+               dtc2 = Datetime_col, 
+               TimeThreshold = Threshold, 
+               overlap = allow_overlap)
+  
   do.call(rbind, tmp)
    
 }
--- tests/test_tag_tales.R
+++ tests/test_tag_tales.R
@@ -1,14 +1,50 @@
 # test tag_tales
+
+# the prior version of tag_tales allowed for overlapping detection histories within its summary.  That is, arrivals at one receiver could fall within the interval of an arrival and departure at a different receiver. 
+
+# e.g., before we might have visit A 1-2pm, and visit B 1:30-2:30.  
+
+# overlaps = TRUE - A 1-2pm, B 1:30-2:30
+# A 1-1:15 pm, B1:30-2:30, A 1:35 - 2:15
+
+
+
+#Without overlaps, we have A 1-1:30, B 1:30 - 1:45, A 1:45-2, B 2-2:30
+
+# working out redRowFun:
+set.seed(1)
+x = runif(6)
+x
+thresh = 0.25
+abs(diff(x)) > thresh # should be F, F, T, T, T
+
+breakup_vector = as.factor(cumsum(c(0, abs(diff(x))) > thresh)) # should be: 0, 0, 0, 1, 2, 3
+breakup_vector
+
+# this is within a station visit - one station, do we need multiple visits?  This says yes - so we need to call the breakup vector again on the values within 
+
+
+
 library(telemetry)
 
 d = readRDS("tests/FishID_568.YBUS_test_tagtales.rds")
-d[order(d$DateTime_PST), ]
+d[order(d$DateTime_PST), ] # expected results: Verona arrival/departure same, Sacd.4 arrival/departure same, then arrival at Mal10.b @ 00:47:24, and departure at 00:51:06.
+
+
+splitFishStationVisits(d, "GEN", 60*60, dtc2 = "DateTime_PST")
+
 
 tag_tales(d, "TagID", "GEN", "DateTime_PST", allow_overlap = TRUE)
 
 tag_tales(d, "TagID", "GEN", "DateTime_PST", allow_overlap = FALSE)
 
 
+d2 = read.csv("tests/test-overlaps.csv")
+
+d2$DateTime_PST = lubridate::ymd_hms(d2$DateTime_PST)
+
+tag_tales(d2, "TagID", "GEN", "DateTime_PST", allow_overlap = TRUE)
+tag_tales(d2, "TagID", "GEN", "DateTime_PST", allow_overlap = FALSE)
 
 
 
