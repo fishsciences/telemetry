c6ee87439e294dd6169b78dd721f435a728f05f8
Myfanwy <myfanwy.johnston@fishsciences.net>
2022-10-11 13:57:10 -0700

    refactor find_orphans

--- R/find_orphans.R
+++ R/find_orphans.R
@@ -2,26 +2,39 @@
 #' @param detections_df a dataframe of detections
 #' @param deployments_df a data frame of receiver deployments
 #' @param fuzzy_match desired time threshold to expand the deployment windows by in order to accommodate slight mismatches between deployment windows and detections
-#' @details relies on data.table::foverlaps() to accomplish the merge.  Warnings will issue if you have detections with no matching deployments, or if you have detections with multiple matching deployments (some of which are probably caused by the fuzzy_match).
+#' @param ... arguments passed on to data.table::foverlaps()
+#' @details relies on data.table::foverlaps() to accomplish the merge.  Warnings will issue if you have detections with no matching deployments, or if you have detections with multiple matching deployments (some of which are caused by the fuzzy_match).
 #' @return data frame of detections with deployment data brought in, where NAs in the ExpandedStart and ExpandedEnd columns indicate orphaned detections.
 #' @author Myfanwy Johnston
 #' @export
 #' 
-find_orphans <- function(detections_df, deployments_df,  fuzzy_match = 60*60, ...) {
+find_orphans <- function(detections_df, deployments_df, 
+                         deployment_start_col = "DeploymentStart",
+                         deployment_end_col = "DeploymentEnd",
+                         deployment_rec_col = "Receiver",
+                         detections_df_datetime_col = "DateTimePST",
+                         fuzzy_match = 60*60,
+                         ...) {
   
   x = as.data.table(detections_df)
   y = as.data.table(deployments_df)
   
-  y$ExpandedStart = y$DeploymentStart - fuzzy_match
-  y$ExpandedEnd = y$DeploymentEnd + fuzzy_match
+ # if(any(c("ExpandedStart", "ExpandedEnd") %in% colnames(y))) 
   
-  x$end = detections_df$DateTimePST 
+  y$ExpandedStart = y[[deployment_start_col]] - fuzzy_match
+  y$ExpandedEnd = y[[deployment_end_col]] + fuzzy_match
   
-  setkey(y, Receiver, ExpandedStart, ExpandedEnd)
-  result = foverlaps(x, y, by.x = c('Receiver', 'DateTimePST', 'end'), type = 'within', ...)
+  x$end = detections_df[[detections_df_datetime_col]]
+  
+  setkeyv(y, cols = c(deployment_rec_col, "ExpandedStart", "ExpandedEnd"))
+          
+  result = foverlaps(x, y, 
+                     by.x = c(deployment_rec_col, detections_df_datetime_col, 'end'), 
+                     type = 'within', 
+                     ...)
   result <- as.data.frame(result)
   result$end = NULL
-  if(sum(is.na(result$DeploymentStart)) > 0){
+  if(sum(is.na(result[[deployment_start_col]])) > 0){
     warning("warning: the resulting dataframe contains NAs in the joining columns - these indicate orphan detections")
   }
   if(nrow(detections_df) < nrow(result)) warning("warning: some detections have multiple matches in the deployment records - check the number of resulting rows against detections_df")
--- tests/test_find_orphans.R
+++ tests/test_find_orphans.R
@@ -2,7 +2,7 @@
 # Fri Oct  7 11:56:05 2022 America/Los_Angeles ------------------------------
 library(telemetry)
 library(data.table)
-load("inst/test_find_orphans.rda") # test data from a single receiver in the WST synthesis dataset.  Includes its full deployment history and a subset of detections from a subset of tagged fish.  Should contain 7 orphan detections w/ no fuzzy match.
+load(system.file(package = "telemetry", "test_find_orphans.rda")) # test data from a single receiver in the WST synthesis dataset.  Includes its full deployment history and a subset of detections from a subset of tagged fish.  Should contain 7 orphan detections w/ no fuzzy match.
 
 # this function should issue a warning for this test data
 testthat::expect_warning(find_orphans(test_dets, test_deps, fuzzy_match = 0))
@@ -22,3 +22,30 @@ testthat::expect_false(any(duplicated(test_dets[ , c("TagID", "DateTimePST")])))
 # there should be duplicates (mult matches) in the return with fuzzy_match != 0
 stopifnot(any(duplicated(ans2[ , c("TagID", "DateTimePST")])))
 
+# test with different column names
+copy = test_dets
+copy$DT = copy$DateTimePST
+copy$DateTimePST = NULL # remove that default col
+
+copydeps = test_deps
+copydeps$DS = copydeps$DeploymentStart
+copydeps$DeploymentStart = NULL
+
+testthat::expect_error(find_orphans(copy, 
+                    copydeps,
+                    deployment_start_col = "DeploymentStart",
+                    deployment_end_col = "DeploymentEnd",
+                    deployment_rec_col = "Receiver",
+                    detections_df_datetime_col = "DT",
+                    fuzzy_match = 60*60
+                    ))
+
+ans3 = find_orphans(
+  copy,
+  copydeps,
+  deployment_start_col = "DS",
+  deployment_end_col = "DeploymentEnd",
+  deployment_rec_col = "Receiver",
+  detections_df_datetime_col = "DT",
+  fuzzy_match = 60 * 60
+)
