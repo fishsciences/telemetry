1f545560d6c9463dda224255ad76930a8b65cf9f
Matt Espe <mespe@ucdavis.edu>
2023-04-24 15:54:16 -0600

    Expands test suite for tag_tales().

--- tests/test_tag_tales.R
+++ tests/test_tag_tales.R
@@ -10,36 +10,56 @@
 library(telemetry)
 
 # test one fish
-d = readRDS("FishID_568.YBUS_test_tagtales.rds")
-d[order(d$DateTime_PST), ] # expected results: Verona arrival/departure same, Sacd.4 arrival/departure same, then arrival at Mal10.b @ 00:47:24, and departure at 00:51:06.
+#d = readRDS("FishID_568.YBUS_test_tagtales.rds")
+#d[order(d$DateTime_PST), ] # expected results: Verona arrival/departure same, Sacd.4 arrival/departure same, then arrival at Mal10.b @ 00:47:24, and departure at 00:51:06.
 
-tag_tales(d, "TagID", "GEN", "DateTime_PST") # add stopifnot(identical()) test here to compare static inputs/outputs
-tag_tales(d, d$TagID, d$GEN, "DateTime_PST")
+#tag_tales(d, "TagID", "GEN", "DateTime_PST") # add stopifnot(identical()) test here to compare static inputs/outputs
+#tag_tales(d, d$TagID, d$GEN, "DateTime_PST")
 # tag_tales(d, d$TagID, d$GEN, d$DateTime_PST) # make error better
 
 x = readRDS(system.file(package = "telemetry", "10.ybus_test_data.rds"))
 x = x[order(x$DateTime_PST), ]
 y = tag_tales(x, "FishID", "GEN", Datetime_col = "DateTime_PST")
 y
+
+
 # test multiple fish
 m = readRDS(system.file(package = "telemetry", "ac_test.rds"))
-z = tag_tales(m, m$Transmitter, m$StationName)
+z = tag_tales(m, m$Transmitter)
 
 # Check for bug in issue #2
 # get first row for each fish
 
 tt = by(m, m$Transmitter, function(df){
-    df[which.min(df$DetectionDateTimeUTC8),]
+    df[which.min(df$DetectionDateTimeUTC),]
 })
 
 # first visit in result
 t2 = by(z, z$Transmitter, function(df){
-    df[which.min(df$DetectionDateTimeUTC8),]
+    df[which.min(df$arrival),]
 })
 
 #They should match
 all(names(tt) == names(t2))
 
+# The station name and times should match for the earliest detection
 stopifnot(all(sapply(tt, `[[`, "StationName") == sapply(t2, `[[`, "StationName")))
-stopifnot(all(sapply(tt, `[[`, "DetectionDateTimeUTC8") == sapply(t2, `[[`, "arrival"))) # This fails when the visist are arbtirary
-         
+stopifnot(all(sapply(tt, `[[`, "DateTimeUTC") == sapply(t2, `[[`, "arrival"))) # This fails when the visist are arbtirary
+
+# Check that they all have the right stations
+tt = tapply(m$StationName, m$Transmitter, unique)
+
+zs = split(z, z$Transmitter)
+
+stopifnot(all(mapply(function(a, b){
+    all(b %in% a$StationName)
+}, zs[names(tt)], tt)))
+
+# Make sure time stamps are valid
+ms = split(m, m$Transmitter)
+
+stopifnot(mapply(function(a, b){
+    all(a$arrival %in% b$DateTimeUTC)},
+    zs[names(ms)], ms))
+    
+
